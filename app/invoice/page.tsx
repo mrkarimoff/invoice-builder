cc382418d7b57c44c4f3a6c756d3c51190741e36
mrkarimoff <mrkarimoff99@gmail.com>
2023-11-26 19:11:33 +0500

    added revalide 0

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -1,6 +1,8 @@
 import InvoiceView from '@/app/invoice/_components/invoiceView';
 import { getAllItems, getInvoiceDetails } from '../_actions/actions';
 
+export const revalidate = 0;
+
 const Page = async () => {
   const itemsResult = await getAllItems();
   const detailsResult = await getInvoiceDetails();
