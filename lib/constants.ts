9317a306ee1b4170ea3026ec9fe0dd2e5b48ed03
mrkarimoff <mrkarimoff99@gmail.com>
2023-12-25 19:08:23 +0500

    update templateData for invoice details

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,9 @@
 export const templateData = {
-  senderName: 'Mirfayz Karimov',
-  address: ['Tor-tor mahalla 2', 'Bukhara, Uzbekistan 200407'],
-  email: 'mirfayzkarimoff@gmail.com',
+  senderName: 'Shoqosim Shomurodov',
+  address: ['Afshor mahalla 2', 'Bukhara, Uzbekistan 200400'],
+  email: 'shoqosim@gmail.com',
   receiverName: 'Northwestern University',
-  invoiceId: '#3',
+  invoiceId: '#1',
   issueDate: '10/01/2023',
   dueDate: 'upon receipt',
   invoicePurpose: 'dates worked the previous month (10/02/2023 -10/31/2023)',
